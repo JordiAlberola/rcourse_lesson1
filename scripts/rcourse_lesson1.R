37b066620cbb5d3813508a1b1f5a990ec4a5ec08
Jordi Alberola Domingo <1000080@uab.cat>
2018-11-14 16:09:49 +0100

    2nd commit

---
+++ scripts/rcourse_lesson1.R
@@ -0,0 +1,29 @@
+#### 1. MAKING A NEW PROJECT ####
+
+# 1.1. Make a directory that will act as our root folder for the project and will store everything we’re going to do in RStudio.
+
+# Create using your operating system a root folder called “rcourse_lesson1” and then inside of it four folders: 1) “data”, 2) “figures”, 3) “scripts”, and 4) “write_up”.
+
+# 1.2 Make a new Project.
+
+# Go to the top right hand corner of RStudio and click on where it says “Project: (None)” and then choose “New Project…”.
+
+# Choose “Existing Directory”. Then use the “Browse…” button to find our root folder, then click “Create Project”.
+
+# 1.3. Commit to Git.
+
+# Go to the top right hand corner that you went to originally to make your project. It should now say the name of your project (for example, mine says “rcourse_lesson1”). Click on it and then choose “Project Options…”
+
+# Choose “Git/SVN” and change the setting of “Version control system” from “(None)” to “Git”, then asking if it is okay to restart RStudio, say “Yes”.
+
+# Click on the sideways “Git” in the upper menu bar and choose “Commit…”.
+
+# Click the box under “Staged” or select an item and then click “Stage” in the top menu.
+
+# Write a message in the window for “Commit message”. Generally for my first commit I just write “Initial commit.” as is done below. When you’re ready click “Commit”, and after click "Close". The box that previously listed our files is now empty, that’s because you have nothing new to commit.
+
+# 1.4. Push to Github
+
+
+
+
